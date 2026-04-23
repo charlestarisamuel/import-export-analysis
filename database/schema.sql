464a425ecb88d0f663d9a3f51b225c0207f16501
Samuel Charles <charlestarisamuel@gmail.com>
2026-04-23 11:27:00 +0300

    Added another function. And caught a core lesson on imports, os, dotenv, sqlalchemy.

--- database/schema.sql
+++ database/schema.sql
@@ -3,10 +3,15 @@
 /*We first drop all the tables in reverse order so that whenever another script runs this file it first deletes the tables before creating them agian because postgres wouldn't let us create tables if they already exist*/
 /*We drop the tables in reverse order of their dependencies if not postgres would send us an error*/
 DROP TABLE IF EXISTS external_signals;
+
 DROP TABLE IF EXISTS costs;
+
 DROP TABLE IF EXISTS shipments;
+
 DROP TABLE IF EXISTS routes;
+
 DROP TABLE IF EXISTS ports;
+
 DROP TABLE IF EXISTS suppliers;
 
 CREATE TABLE suppliers (
@@ -21,8 +26,10 @@ CREATE TABLE ports (
     port_id INT PRIMARY KEY NOT NULL,
     port_name VARCHAR(225) NOT NULL,
     country VARCHAR(50) NOT NULL,
-    port_type VARCHAR(10) CHECK (port_type IN ('sea', 'air', 'land')), /*this is how we write it when we want the inputs only be this tree values*/
-    avg_delays_days DECIMAL(3,1)
+    port_type VARCHAR(10) CHECK (
+        port_type IN ('sea', 'air', 'land')
+    ),
+    /*this is how we write it when we want the inputs only be this tree values*/ avg_delays_days DECIMAL(3, 1)
 );
 
 CREATE TABLE routes (
@@ -32,8 +39,8 @@ CREATE TABLE routes (
     distance_km DECIMAL(10, 2),
     typical_duration_days INT,
     base_cost_usd DECIMAL(10, 2),
-    FOREIGN KEY (origin_port_id) REFERENCES ports(port_id),
-    FOREIGN KEY (destination_port_id) REFERENCES ports(port_id)
+    FOREIGN KEY (origin_port_id) REFERENCES ports (port_id),
+    FOREIGN KEY (destination_port_id) REFERENCES ports (port_id)
 );
 
 CREATE TABLE shipments (
@@ -47,14 +54,22 @@ CREATE TABLE shipments (
     actual_arrival DATE,
     cargo_type VARCHAR(50),
     weight_kg DECIMAL(10, 2),
-    status VARCHAR(20) CHECK (status IN ('delivered', 'in transit', 'delayed', 'arrived', 'cancelled')),  
-    FOREIGN KEY (supplier_id) REFERENCES suppliers(supplier_id),
-    FOREIGN KEY (route_id) REFERENCES routes(route_id),
-    FOREIGN KEY (origin_port_id) REFERENCES ports(port_id),
-    FOREIGN KEY (destination_port_id) REFERENCES ports(port_id) 
+    status VARCHAR(20) CHECK (
+        status IN (
+            'delivered',
+            'in transit',
+            'delayed',
+            'arrived',
+            'cancelled'
+        )
+    ),
+    FOREIGN KEY (supplier_id) REFERENCES suppliers (supplier_id),
+    FOREIGN KEY (route_id) REFERENCES routes (route_id),
+    FOREIGN KEY (origin_port_id) REFERENCES ports (port_id),
+    FOREIGN KEY (destination_port_id) REFERENCES ports (port_id)
 );
 
-CREATE TABLE costs(
+CREATE TABLE costs (
     cost_id INT PRIMARY KEY NOT NULL,
     shipment_id INT NOT NULL,
     fuel_cost DECIMAL(10, 2) NOT NULL,
@@ -62,7 +77,7 @@ CREATE TABLE costs(
     supplier_fee DECIMAL(10, 2) NOT NULL,
     customs_cost DECIMAL(10, 2) NOT NULL,
     miscellaneous DECIMAL(10, 2),
-    FOREIGN KEY (shipment_id) REFERENCES shipments(shipment_id)
+    FOREIGN KEY (shipment_id) REFERENCES shipments (shipment_id)
 );
 
 CREATE TABLE external_signals (
@@ -72,7 +87,7 @@ CREATE TABLE external_signals (
     fuel_price_usd DECIMAL(10, 2) NOT NULL,
     weather_risk_score DECIMAL(3, 1),
     notes TEXT,
-    FOREIGN KEY (route_id) REFERENCES routes(route_id)
+    FOREIGN KEY (route_id) REFERENCES routes (route_id)
 );
 
 /*notice routes references ports, and shipments references both routes and suppliers. The order your tables are written matters — PostgreSQL can't create a table that references another table that doesn't exist yet.*/
--- src/analytics.py
+++ src/analytics.py
@@ -1,8 +1,19 @@
-from src.db_connection import get_connection
+from sqlalchemy import create_engine
 import pandas as pd
+import os
+from dotenv import load_dotenv
+
+load_dotenv() #loads the environment variables from the .env file into the system environment variables, making them accessible via os.getenv()#
+
+
+def get_engine():
+    engine = create_engine(
+        f"postgresql+psycopg2://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}/{os.getenv('DB_NAME')}"
+    )
+    return engine
 
 def get_total_cost_per_shipment(): #notice that naming your functions properly are important too, the should be self explanatory non of that coded bs that is unnecessary#
-    conn = get_connection() #establish you database connection then write your query below#
+    conn = get_engine()
     query = """
         SELECT s.shipment_id, s.cargo_type, s.status,
                c.fuel_cost + c.port_fees + c.supplier_fee + c.customs_cost + COALESCE(c.miscellaneous, 0) AS total_cost
@@ -11,11 +22,29 @@ def get_total_cost_per_shipment(): #notice that naming your functions properly a
         ORDER BY total_cost DESC;
     """
     df = pd.read_sql(query, conn) #runs the query and loads result into the panda dataframe. It takes the query and the database connection as arguments#
-    conn.close() #always close the connection when done#
+    #we dont need to close the connection because sqlalchemy handles it automatically#
     return df
 
+def get_avg_cost_per_route():
+    conn = get_engine()
+    query = """
+        SELECT r.route_id,
+                p1.port_name AS origin_port,
+                p2.port_name AS destination_port,
+                ROUND(AVG(c.fuel_cost + c.port_fees + c.supplier_fee + c.customs_cost + COALESCE(c.miscellaneous, 0)), 2) AS avg_total_cost
+        FROM routes r
+        JOIN shipments s ON r.route_id = s.route_id
+        JOIN costs c ON s.shipment_id = c.shipment_id
+        JOIN ports p1 ON r.origin_port_id = p1.port_id
+        JOIN ports p2 ON r.destination_port_id = p2.port_id
+        GROUP BY r.route_id, p1.port_name, p2.port_name
+        ORDER BY avg_total_cost DESC;
+    """
+    df = pd.read_sql(query, conn)
+    return df 
+
 if __name__ == "__main__":
-    df = get_total_cost_per_shipment()
+    df = get_avg_cost_per_route()
     print(df)
 
 
